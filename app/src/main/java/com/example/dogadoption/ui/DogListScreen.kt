fefe6d8b050cc4e1cc827ef44ddf828c9eabcb74
Filip <29filip@gmail.com>
2024-04-04 19:27:29 +0100

    stage 2 commit

--- app/src/main/java/com/example/dogadoption/ui/DogListScreen.kt
+++ app/src/main/java/com/example/dogadoption/ui/DogListScreen.kt
@@ -3,6 +3,7 @@ package com.example.dogadoption.ui
 import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,6 +12,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -24,6 +28,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -76,16 +81,26 @@ fun DogListScreen(navController: NavController) {
     ) {
         Column {
             Spacer(Modifier.padding(32.dp))
-            LazyColumn {
+            LazyColumn(
+                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+            ) {
                 items(dogBreeds) { breed ->
-                    Text(breed,
-                        Modifier
+                    Card(
+                        colors = CardDefaults.cardColors(containerColor = Color.White),
+                        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp, vertical = 8.dp)
                             .fillMaxWidth()
-                            .clickable {navController.navigate("DogPicsScreen/$breed")}
-                            .padding(16.dp),
-                        textAlign = TextAlign.Center,
-                        fontSize = 20.sp
-                    )
+                    ) {
+                        Text(breed,
+                            Modifier
+                                .fillMaxWidth()
+                                .clickable { navController.navigate("DogPicsScreen/$breed") }
+                                .padding(16.dp),
+                            textAlign = TextAlign.Center,
+                            fontSize = 20.sp
+                        )
+                    }
                 }
             }
         }
